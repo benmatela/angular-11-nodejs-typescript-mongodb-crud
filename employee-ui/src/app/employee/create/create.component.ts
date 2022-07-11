5f318b02d6786bb34ca3f2a22cc131de2e70baf5
Ben Matela <bmatela@afrogeek.co.za>
2022-07-11 11:27:42 +0200

    refactor employee form

--- employee-ui/src/app/employee/create/create.component.ts
+++ employee-ui/src/app/employee/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
